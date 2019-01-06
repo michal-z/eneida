61fb4b1f4b05deba8b1b89d7f71bd35d181ea4a9
michal-z <michalziulek@gmail.com>
2019-01-06 19:43:59 +0100

    cleanups

--- test01/make.bat
+++ test01/make.bat
@@ -1,30 +1,19 @@
 @echo off
 
 set CFLAGS=/Od /D_DEBUG
-::set CFLAGS=/O2 /GL /Oi /fp:fast
+::set CFLAGS=/O2 /fp:fast
 
 set CC=..\compiler\cl.exe
-set HLSL=..\compiler\fxc.exe /Ges /O3 /WX /nologo /Qstrip_reflect /Qstrip_debug /Qstrip_priv
-set ASM=..\compiler\fasm.exe
+
 set APPNAME=test01
 
 if exist %APPNAME%.exe del %APPNAME%.exe
 
-::%HLSL% /D VS_E01_TRANSFORM /E vs_e01_transform /Fo vs_e01_transform.cso /T vs_5_1 experiment01.hlsl & if errorlevel 1 goto :end
-::%HLSL% /D PS_E01_SHADE /E ps_e01_shade /Fo ps_e01_shade.cso /T ps_5_1 experiment01.hlsl & if errorlevel 1 goto :end
+%CC% %CFLAGS% /Gm- /nologo /GS- /Gs999999 /Gy /Gw /EHa- /Bt /WX /Wall /wd4710 /wd4711 /wd5045 /wd4820 /wd4201 /wd4221 /wd4152 /wd4204 %APPNAME%.c ^
+     /link ..\compiler\kernel32.lib /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /ENTRY:Start /NODEFAULTLIB /OUT:%APPNAME%.exe
 
-::%ASM% asmlib.asm & if errorlevel 1 goto :end
-
-%CC% %CFLAGS% /Gm- /nologo /GS- /Gs999999 /Gy /Gw /EHa- ^
-    /WX /Wall /Bt /wd4710 /wd4711 /wd5045 /wd4820 /wd4201 /wd4221 /wd4152 /wd4204 ^
-    %APPNAME%.c ^
-    /link ..\compiler\kernel32.lib ^
-    /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /ENTRY:Start /NODEFAULTLIB /OUT:%APPNAME%.exe
+if exist *.obj del *.obj
 
 :end
-if exist *.obj del *.obj
-if exist *.cso del *.cso
 
 if "%1" == "run" if exist %APPNAME%.exe (.\%APPNAME%.exe)
-::if "%1" == "debug" if exist %APPNAME%.exe (WinDbgX /y .\ /srcpath .\ .\%APPNAME%.exe)
-if "%1" == "debug" if exist %APPNAME%.exe (WinDbgX .\%APPNAME%.exe)
