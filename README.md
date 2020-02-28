c5513f4eaefab59327b3fa58f6108a96dab6abbe
michal-amd <michalziulek@gmail.com>
2020-02-28 16:08:53 +0100

    -

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-#eneida
+# eneida
 
-## #Project goals
-    : <br /> *Create interesting,
-      real - time animations by writing simple,
-      low - level code.*Explore math,
-      procedural generation techniques and audio programming.*Eventually,
-      create a demoscene production and submit it to a demo party.
+### Project goals:<br />
+* Create interesting, real-time animations by writing simple, low-level code.
+* Explore math, procedural generation techniques and audio programming.
+* Eventually, create a demoscene production and submit it to a demo party.
--- alib.asm
+++ alib.asm
@@ -1,32 +1,42 @@
 format MS64 COFF
 
-    public memset public memcpy public sqrtf
+public memset
+public memcpy
+public sqrtf
 
-        macro defshader
-sh{sh : file `sh #'.cso' sh #_size dd $ - sh public sh public sh #_size}
+macro defshader sh {
+  sh: file `sh#'.cso'
+  sh#_size dd $-sh
+  public sh
+  public sh#_size
+}
 
 section '.text' code readable executable
 
-    ;
-TODO(mziulek)
-    : This is very slow.memset : xor eax,
-eax .0 : mov[rcx + rax], dl inc rax dec r8 jnz .0 mov rax, rcx ret
+; TODO(mziulek): This is very slow.
+memset:     xor         eax, eax
+.0:         mov         [rcx+rax], dl
+            inc         rax
+            dec         r8
+            jnz         .0
+            mov         rax, rcx
+            ret
 
-    ;
-TODO(mziulek)
-    : This is very slow.memcpy : xor eax,
-eax .0 : mov r10b,
-         [rdx + rax] mov[rcx + rax],
-         r10b inc rax dec r8 jnz .0 mov rax,
-         rcx ret
+; TODO(mziulek): This is very slow.
+memcpy:     xor         eax, eax
+.0:         mov         r10b, [rdx+rax]
+            mov         [rcx+rax], r10b
+            inc         rax
+            dec         r8
+            jnz         .0
+            mov         rax, rcx
+            ret
 
-    ;
-TODO(mziulek)
-    : Add some debug stuff if needed.sqrtf : sqrtss xmm0,
-xmm0 ret
+; TODO(mziulek): Add some debug stuff if needed.
+sqrtf:      sqrtss      xmm0, xmm0
+            ret
 
-    section '.data' data readable writeable
+section '.data' data readable writeable
 
-    ;
-defshader vs_e01_transform;
-defshader ps_e01_shade
+;defshader vs_e01_transform
+;defshader ps_e01_shade
