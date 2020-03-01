2ab58ff50e649f65a05e665bd6a7ce8c3187aa9c
michal-z <michalziulek@gmail.com>
2020-03-01 22:26:17 +0100

    -

--- make.bat
+++ make.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%1" == "fmt" (
-    "build\clang-format.exe" -i *.c *.h ^
-    -style="{ColumnLimit: 100}"
-    goto :end
+  "build\clang-format.exe" -i *.c *.h ^
+  -style="{ColumnLimit: 100, BasedOnStyle: LLVM}"
+  goto :end
 )
 
 set CFLAGS=/Od /D_DEBUG
@@ -15,9 +15,15 @@ set APPNAME=eneida
 if exist %APPNAME%.exe del %APPNAME%.exe
 
 %CC% /Zi %CFLAGS% /nologo /GS- /Gs999999 /Gy /Gw /EHa- /W4 ^
-    %APPNAME%.c libs.c ^
-    /link build\kernel32.lib /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /ENTRY:start /NODEFAULTLIB ^
-    /OUT:%APPNAME%.exe
+  %APPNAME%.c libs.c ^
+  /link build\kernel32.lib /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /ENTRY:start /NODEFAULTLIB ^
+  /OUT:%APPNAME%.exe
 
 :end
 if exist *.obj del *.obj
+
+if "%1" == "run" (
+  if exist %APPNAME%.exe (
+    %APPNAME%.exe
+  )
+)
