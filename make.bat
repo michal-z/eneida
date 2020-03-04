48eac29c58b2d5c5da742d22cfa042ec2a3e3836
michal-z <michalziulek@gmail.com>
2020-03-04 20:35:20 +0100

    -

--- make.bat
+++ make.bat
@@ -14,7 +14,7 @@ set APPNAME=eneida
 
 if exist %APPNAME%.exe del %APPNAME%.exe
 
-%CC% -fuse-ld=lld -nobuiltininc -v %CFLAGS% /nologo /GS- /Gs16384 /X /Gy /Gw /GR- /EHa- /I. /W4 ^
+%CC% -fuse-ld=lld -nobuiltininc %CFLAGS% /nologo /GS- /Gs16384 /X /Gy /Gw /GR- /EHa- /I. /W4 ^
   %APPNAME%.c libs.c ^
   /link build\kernel32.lib /opt:ref /incremental:no /subsystem:windows /entry:start /nodefaultlib ^
   /OUT:%APPNAME%.exe
